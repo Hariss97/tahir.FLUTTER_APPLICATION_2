1e5145b75bf7dd234f950cba8fa9f3362e5f3b51
engrhariss <engrharissabt@gmail.com>
2025-02-02 17:24:50 +0100

    initialised a new project

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Scaffold(
+      body: SafeArea(child: Column(
+        children: [
+          Text("This is a very Nice App"),
+        ],
+      )),
+    ),
+  ));
+}
